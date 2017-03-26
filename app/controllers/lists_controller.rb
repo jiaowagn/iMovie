95c9e265923336d3714b56735a73c0481a05854b
jiaowagn <wangjiaohere@gmail.com>
2017-03-26 22:50:02 +0800

    implement lists#create

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -5,5 +5,18 @@ class ListsController < ApplicationController
 
   def new
     @list = List.new
-  end 
+  end
+
+  def create
+    @list = List.new(list_params)
+    @list.save
+
+    redirect_to lists_path
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:title, :profile)
+  end
 end
