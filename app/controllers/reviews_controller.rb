16d441b05a05fb68484a366244e93369be6df14d
jiaowagn <wangjiaohere@gmail.com>
2017-03-27 21:58:53 +0800

    implement review create function

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,28 @@
 class ReviewsController < ApplicationController
+  before_action :authenticate_user!, :only => [:new, :create]
+
+  def new
+    @list = List.find(params[:list_id])
+    @review = Review.new
+  end
+
+  def create
+    @list = List.find(params[:list_id])
+    @review = Review.new(review_params)
+    @review.list = @list
+    @review.user = current_user
+
+    if @review.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content)
+  end
+  
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :user
-  belongs_to :list 
+  belongs_to :list
+
+  validates :content, presence: true 
 end
