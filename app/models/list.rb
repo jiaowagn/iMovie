fad49997d7c9162c8bc886733147dd063c46c545
jiaowagn <wangjiaohere@gmail.com>
2017-03-27 21:06:45 +0800

    add review and hook list/user relation

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,4 +1,5 @@
 class List < ApplicationRecord
-  belongs_to :user 
+  belongs_to :user
+  has_many :reviews 
   validates :title, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :lists 
+  has_many :lists
+  has_many :reviews 
 end
