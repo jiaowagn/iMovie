3d9bdfeb526842967d30460c0ca0d2148c59b8c5
jiaowagn <wangjiaohere@gmail.com>
2017-03-27 23:48:23 +0800

    implement my reviews

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,8 @@ Rails.application.routes.draw do
 
   namespace :account do
     resources :lists
-  end 
+    resources :reviews 
+  end
   root 'lists#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
