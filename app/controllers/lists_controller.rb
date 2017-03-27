d4b3d2180fe6e8ebfe5d88364b2d5854f3254db6
jiaowagn <wangjiaohere@gmail.com>
2017-03-27 23:01:01 +0800

    join & quit list action

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -41,6 +41,31 @@ class ListsController < ApplicationController
     redirect_to lists_path
   end
 
+  def join
+    @list = List.find(params[:id])
+
+    if !current_user.is_member_of?(@list)
+      current_user.join!(@list)
+      flash[:notice] = "成功收藏该影片！"
+    else
+      flash[:warning] = "您已经收藏了该影片！"
+    end
+
+    redirect_to list_path(@list)
+  end
+
+  def quit
+    @list = List.find(params[:id])
+    if current_user.is_member_of?(@list)
+      current_user.quit!(@list)
+      flash[:alert] = "已经取消收藏该影片！"
+    else
+      flash[:warning] = "您没有收藏该影片，怎么取消收藏？"
+    end
+
+    redirect_to list_path(@list)
+  end 
+
   private
 
   def find_list_and_check_permission
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :lists do
+    member do
+      post :join
+      post :quit
+    end
+    
     resources :reviews
-  end 
+  end
   root 'lists#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
