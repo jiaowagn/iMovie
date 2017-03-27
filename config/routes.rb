eb9459e4556c6c60182abae744b28827dd40c7a1
jiaowagn <wangjiaohere@gmail.com>
2017-03-27 21:10:41 +0800

    add reviews nested route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :lists
+  resources :lists do
+    resources :reviews
+  end 
   root 'lists#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
